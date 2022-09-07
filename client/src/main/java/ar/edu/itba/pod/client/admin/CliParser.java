a5d3775b14659fa9c17c7d5c7efe62ed570642f8
Lucia Torrusio <45410086+luciatorrusio@users.noreply.github.com>
2022-09-07 10:00:32 -0300

    bring master to assignseat (#48)
    * [Feature] Added Admin client argument parser and tests (#39)
    
    * 26 adminclient models (#40)
    
    * [Feature] Implemented models action
    
    * [Feature] Added tests
    
    * [Feature] Added client command and tests (#44)
    
    * AdminClient: confirm (#45)
    
    * [Feature] Added parser and tests
    
    * [Feature] Added Confirm action
    
    * [Feature] Added tests for action
    
    * AdminClient: Cancel (#46)
    
    * [Feature] Added cliParser
    
    * [Feature] Added cli parser tests
    
    * [Feature] Added cancel action and tests
    
    * AdminClient: Flights (#47)
    
    * [Feature] Added parser and tests
    
    * [Feature] Implemented action and tests
    
    * [Fix] fixed broken test
    
    Co-authored-by: Lucas Dell'Isola <dev@lucasdellisola.com.ar>

---
+++ client/src/main/java/ar/edu/itba/pod/client/admin/CliParser.java
@@ -0,0 +1,155 @@
+package ar.edu.itba.pod.client.admin;
+
+import ar.edu.itba.pod.client.admin.actions.ActionType;
+import org.apache.commons.cli.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.Optional;
+import java.util.regex.Pattern;
+
+public class CliParser {
+
+    private final Logger logger =LoggerFactory.getLogger(ar.edu.itba.pod.client.Client.class);
+    private final CommandLineParser parser = new DefaultParser();
+    private final static Pattern SERVER_ADDRESS_PATTERN = Pattern.compile("^\\d?\\d(?:\\.\\d{1,2}){3}:\\d{1,4}$");
+    private final Options options = new Options();
+
+
+    public Optional<Arguments> parse(String[] args) {
+        try {
+            return GetArguments(parser.parse(options, args));
+        } catch (ParseException e) {
+            logger.error("Error parsing command line arguments");
+            return Optional.empty();
+        }
+    }
+
+    public CliParser(){
+        options.addRequiredOption("DserverAddress","DserverAddress",true,"RMI Server address");
+        options.addRequiredOption("Daction","Daction",true,"Action to perform");
+        options.addOption(new Option("DinPath","DinPath",true,"Input file path"))
+                .addOption(new Option("Dflight","Dflight",true,"Flight code"));
+
+    }
+
+    private Optional<Arguments> GetArguments(CommandLine cmd) {
+        var args = new Arguments();
+
+        // Set server address
+        var serverAddress = cmd.getOptionValue("DserverAddress");
+        if (SERVER_ADDRESS_PATTERN.matcher(serverAddress).matches())
+            args.setServerAddress(serverAddress);
+        else {
+            logger.error("The server address is not valid!");
+            return Optional.empty();
+        }
+
+        // Set action
+        try {
+            args.setAction(ActionType.valueOf(cmd.getOptionValue("Daction").toUpperCase()));
+        } catch (IllegalArgumentException e) {
+            logger.error("The action is not valid!");
+            return Optional.empty();
+        }
+
+        // Check action and other parameters
+
+        switch (args.getAction()){
+            case MODELS:
+            case FLIGHTS:
+                if (cmd.hasOption("DinPath"))
+                    args.setFilePath(cmd.getOptionValue("DinPath"));
+                else {
+                    logger.error("The input path is required for this action!");
+                    return Optional.empty();
+                }
+                break;
+            case STATUS:
+            case CANCEL:
+            case CONFIRM:
+                if (cmd.hasOption("Dflight"))
+                    args.setFlightCode(cmd.getOptionValue("Dflight"));
+                else {
+                    logger.error("The flight code is required for this action!");
+                    return Optional.empty();
+                }
+                break;
+            case RETICKETING:
+                break;
+        }
+
+        return args.isValid() ? Optional.of(args) : Optional.empty();
+    }
+
+    public static class Arguments{
+        private final Logger logger = LoggerFactory.getLogger(ar.edu.itba.pod.client.admin.CliParser.class);
+        private String serverAddress;
+        private ActionType action;
+        private Optional<String> filePath = Optional.empty();
+        private Optional<String> flightCode = Optional.empty();
+
+
+        public boolean isValid(){
+            if (filePath.isPresent()) {
+
+                var file = new File(filePath.get());
+
+                if (!file.getName().endsWith(".csv")) {
+                    logger.error("The file is not a csv file!");
+                    return false;
+                }
+
+                if (!file.exists()){
+                    logger.error("The file does not exist!");
+                    return false;
+                }
+            }
+
+            if (flightCode.isPresent() && flightCode.get().isEmpty()){
+                logger.error("The flight code is not valid!");
+                return false;
+            }
+
+            return true;
+        }
+
+
+
+
+        public Optional<String> getFilePath() {
+            return filePath;
+        }
+
+        public void setFilePath(String filePath) {
+            this.filePath = Optional.of(filePath);
+        }
+
+        public ActionType getAction() {
+            return action;
+        }
+
+        public void setAction(ActionType action) {
+            this.action = action;
+        }
+
+        public String getServerAddress() {
+            return serverAddress;
+        }
+
+        public void setServerAddress(String serverAddress) {
+            this.serverAddress = serverAddress;
+        }
+
+        public Optional<String> getFlightCode() {
+            return flightCode;
+        }
+
+        public void setFlightCode(String flightCode) {
+            this.flightCode = Optional.ofNullable(flightCode);
+        }
+    }
+
+
+}
---
+++ client/src/test/java/ar/edu/itba/pod/client/admin/CliParserTests.java
@@ -0,0 +1,270 @@
+package ar.edu.itba.pod.client.admin;
+
+import static org.assertj.core.api.Assertions.*;
+
+import ar.edu.itba.pod.client.admin.actions.ActionType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Objects;
+
+public class CliParserTests {
+
+    private CliParser cliParser;
+
+    @BeforeEach
+    public void setUp(){
+        cliParser = new CliParser();
+    }
+
+    @ParameterizedTest()
+    @ValueSource(strings = {"","1..1.1:9999","11.11.11.11","11.123.11.11:9999","hola","11.11.11.11:98900",
+            "11:99","11.11:999","11.11.11.11:10.10"
+    })
+    public void invalidServerAddress_ShouldFail(String serverAddress){
+        // Arrange
+        var args = new String[]{"-DserverAddress=" + serverAddress, "-Daction=flights"};
+
+        // Act
+        var cli = cliParser.parse(args);
+
+        // Assert
+        assertThat(cli.isEmpty()).isTrue();
+    }
+
+    @ParameterizedTest()
+    @ValueSource(strings = {"10.23.34.55:9999","1.2.3.5:9999", "10.23.34.55:999", "10.23.34.55:99", "10.23.34.55:9"})
+    public void validServerAddress_ShouldSucceed(String serverAddress){
+        // Arrange
+        var args = new String[]{"-DserverAddress=" + serverAddress, "-Daction=confirm", "-Dflight=AA123"};
+
+        // Act
+        var cli = cliParser.parse(args);
+
+        // Assert
+        assertThat(cli.isPresent()).isTrue();
+        assertThat(cli.get().getServerAddress()).isEqualTo(serverAddress);
+    }
+
+    @Test()
+    public void noParameters_ShouldFail(){
+        // Arrange
+        var args = new String[]{};
+
+        // Act
+        var cli = cliParser.parse(args);
+         // Assert
+        assertThat(cli.isEmpty()).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"","invalid"})
+    public void invalidAction_ShouldFail(String action) {
+        // Arrange
+        var args = new String[]{"-DserverAddress=10.23.34.55:9999", "-Daction=" + action};
+
+        // Act
+        var cli = cliParser.parse(args);
+
+        // Assert
+        assertThat(cli.isEmpty()).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "reticketing", "Reticketing"
+    })
+    public void validAction_ShouldSucceed(String action) {
+        // Arrange
+        var args = new String[]{"-DserverAddress=10.23.34.55:9999", "-Daction=" + action};
+
+        // Act
+        var cli = cliParser.parse(args);
+
+        // Assert
+        assertThat(cli.isPresent()).isTrue();
+        assertThat(cli.get().getAction()).isEqualTo(ActionType.valueOf(action.toUpperCase()));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Models","models"})
+    public void parseModels_ShouldSucceed(String action){
+        // Arrange
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        var file = Objects.requireNonNull(classloader.getResource("test.csv")).getPath();
+        var args = new String[]{"-DserverAddress=10.23.34.55:9999", "-Daction=" + action,"-DinPath=" + file};
+
+        // Act
+        var cli = cliParser.parse(args);
+
+        // Assert
+        assertThat(cli.isPresent()).isTrue();
+        assertThat(cli.get().getAction()).isEqualTo(ActionType.MODELS);
+        assertThat(cli.get().getFilePath().isPresent()).isTrue();
+        assertThat(cli.get().getFilePath().get()).isEqualTo(file);
+    }
+
+    @Test
+    public void parseModels_InvalidExtension_ShouldFail(){
+        // Arrange
+        var file = "../../file.exe";
+        var args = new String[]{"-DserverAddress=10.23.34.55:9999", "-Daction=models","-DinPath=" + file};
+
+        // Act
+        var cli = cliParser.parse(args);
+
+        // Assert
+        assertThat(cli.isEmpty()).isTrue();
+    }
+
+    @Test
+    public void parseModels_FileDoesNotExist_ShouldFail(){
+        // Arrange
+        var file = "../../file.csv";
+        var args = new String[]{"-DserverAddress=10.23.34.55:9999", "-Daction=models","-DinPath=" + file};
+
+        // Act
+        var cli = cliParser.parse(args);
+
+        // Assert
+        assertThat(cli.isEmpty()).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"status","Status"})
+    public void parseStatus_ShouldSucceed(String action){
+        // Arrange
+        var flightCode = "AA123";
+        var args = new String[]{"-DserverAddress=10.23.34.55:9999", "-Daction=" + action,"-Dflight=" + flightCode};
+
+        // Act
+        var cli = cliParser.parse(args);
+
+        // Assert
+        assertThat(cli.isPresent()).isTrue();
+        assertThat(cli.get().getAction()).isEqualTo(ActionType.STATUS);
+        assertThat(cli.get().getFlightCode().isPresent()).isTrue();
+        assertThat(cli.get().getFlightCode().get()).isEqualTo(flightCode);
+    }
+
+    @Test
+    public void parseStatus_EmptyFlightCode_ShouldFail(){
+        // Arrange
+        var flightCode = "";
+        var args = new String[]{"-DserverAddress=10.23.34.55:9999", "-Daction=status","-Dflight=" + flightCode};
+
+        // Act
+        var cli = cliParser.parse(args);
+
+        // Assert
+        assertThat(cli.isEmpty()).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"confirm","Confirm"})
+    public void parseConfirm_ShouldSucceed(String action){
+        // Arrange
+        var flightCode = "AA123";
+        var args = new String[]{"-DserverAddress=10.23.34.55:9999", "-Daction=" + action,"-Dflight=" + flightCode};
+
+        // Act
+        var cli = cliParser.parse(args);
+
+        // Assert
+        assertThat(cli.isPresent()).isTrue();
+        assertThat(cli.get().getAction()).isEqualTo(ActionType.CONFIRM);
+        assertThat(cli.get().getFlightCode().isPresent()).isTrue();
+        assertThat(cli.get().getFlightCode().get()).isEqualTo(flightCode);
+    }
+
+    @Test
+    public void parseConfirm_EmptyFlightCode_ShouldFail(){
+        // Arrange
+        var flightCode = "";
+        var args = new String[]{"-DserverAddress=10.23.34.55:9999", "-Daction=confirm","-Dflight=" + flightCode};
+
+        // Act
+        var cli = cliParser.parse(args);
+
+        // Assert
+        assertThat(cli.isEmpty()).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"cancel","Cancel"})
+    public void parseCancel_ShouldSucceed(String action){
+        // Arrange
+        var flightCode = "AA123";
+        var args = new String[]{"-DserverAddress=10.23.34.55:9999", "-Daction=" + action,"-Dflight=" + flightCode};
+
+        // Act
+        var cli = cliParser.parse(args);
+
+        // Assert
+        assertThat(cli.isPresent()).isTrue();
+        assertThat(cli.get().getAction()).isEqualTo(ActionType.CANCEL);
+        assertThat(cli.get().getFlightCode().isPresent()).isTrue();
+        assertThat(cli.get().getFlightCode().get()).isEqualTo(flightCode);
+    }
+
+    @Test
+    public void parseCancel_EmptyFlightCode_ShouldFail(){
+        // Arrange
+        var flightCode = "";
+        var args = new String[]{"-DserverAddress=10.23.34.55:9999", "-Daction=confirm","-Dflight=" + flightCode};
+
+        // Act
+        var cli = cliParser.parse(args);
+
+        // Assert
+        assertThat(cli.isEmpty()).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Flights","flights"})
+    public void parseFlights_ShouldSucceed(String action){
+        // Arrange
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        var file = Objects.requireNonNull(classloader.getResource("test.csv")).getPath();
+        var args = new String[]{"-DserverAddress=10.23.34.55:9999", "-Daction=" + action,"-DinPath=" + file};
+
+        // Act
+        var cli = cliParser.parse(args);
+
+        // Assert
+        assertThat(cli.isPresent()).isTrue();
+        assertThat(cli.get().getAction()).isEqualTo(ActionType.FLIGHTS);
+        assertThat(cli.get().getFilePath().isPresent()).isTrue();
+        assertThat(cli.get().getFilePath().get()).isEqualTo(file);
+    }
+
+    @Test
+    public void parseFlights_InvalidExtension_ShouldFail(){
+        // Arrange
+        var file = "../../file.exe";
+        var args = new String[]{"-DserverAddress=10.23.34.55:9999", "-Daction=flights","-DinPath=" + file};
+
+        // Act
+        var cli = cliParser.parse(args);
+
+        // Assert
+        assertThat(cli.isEmpty()).isTrue();
+    }
+
+    @Test
+    public void parseFlights_FileDoesNotExist_ShouldFail(){
+        // Arrange
+        var file = "../../file.csv";
+        var args = new String[]{"-DserverAddress=10.23.34.55:9999", "-Daction=flights","-DinPath=" + file};
+
+        // Act
+        var cli = cliParser.parse(args);
+
+        // Assert
+        assertThat(cli.isEmpty()).isTrue();
+    }
+
+
+}
