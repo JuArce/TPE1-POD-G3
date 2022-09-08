d875ddc4c1c60ef1527875a6b2c3c7d449054aa5
luciatorrusio <lucia.torrusio@gmail.com>
2022-09-08 15:56:18 -0300

    FInished all tests, changed implementation due to change in models

--- server/src/main/java/ar/edu/itba/pod/server/SeatAssignmentService.java
+++ server/src/main/java/ar/edu/itba/pod/server/SeatAssignmentService.java
@@ -10,20 +10,25 @@ import java.util.Optional;
 public class SeatAssignmentService implements ar.edu.itba.pod.services.SeatAssignmentService {
 
     List<Flight> flightList;
-//    todo levatar la clase de storage en vez de tener el list de flights
+
+    public SeatAssignmentService(List<Flight> flightList) {
+        this.flightList = flightList;
+    }
 
     @Override
     public Boolean isSeatTaken(String flightCode, int row, char column) {
         return null;
     }
-//todo hacer excepciones como clases
     @Override
     public void assignSeat(String flightCode, String passenger, int row, char column) throws Exception {
         if(passenger.isEmpty())
-            throw  new InvalidPassengerException();
-
+            throw new InvalidPassengerException();
+//        TODO: asegurarse que row 1 es el 0
         if(row <= 0 || !Character.isLetter(column))
             throw new SeatDoesNotExistException();
+
+        if(flightCode.isEmpty())
+            throw new FlightDoesNotExistException();
 
         Flight flight = flightList.stream()
                 .filter(f -> f.getFlightCode().equals(flightCode))
@@ -32,24 +37,32 @@ public class SeatAssignmentService implements ar.edu.itba.pod.services.SeatAssig
         if(flight.getStatus() == FlightStatus.CONFIRMED)
             throw new FlightIsConfirmedException();
 
+        if(flight.getStatus() == FlightStatus.CANCELLED)
+            throw new FlightIsCancelledException();
+
         Ticket ticket = flight.getTickets().stream()
                 .filter(t -> t.getPassengerName().equals(passenger))
                 .findAny().orElseThrow(InvalidPassengerException::new);
 
-        if(ticket.getSeat() != null)
+        if(ticket.getSeatLocation().isPresent())
             throw new PassengerAlreadyAssignedException();
-
+        Ticket.SeatLocation seatLocation = new Ticket.SeatLocation(row, column);
         Optional<Ticket> ticketOnLocation = flight.getTickets().stream()
-                .filter(t -> t.getSeat().getLocation().equals(new Pair<>(row, column)))
+                .filter(t -> t.getSeatLocation().isPresent() && t.getSeatLocation().get().equals(seatLocation))
                 .findAny();
+
         if(ticketOnLocation.isPresent())
             throw new SeatAlreadyAssignedException();
+        Plane plane = flight.getPlane();
+        if( ((long) plane.getRows().size() < row) || (plane.getRows().get(row-1).getColumns() < (column-'a' + 1)) )
+            throw new SeatDoesNotExistException();
 
-        //       todo compare categories
-//            throw new RuntimeException("SeatCategoryIsToHighException");
-//        fixme get seat category
-        Seat seat = new Seat(true, passenger,row, column, SeatCategory.BUSINESS );
-        ticket.setSeat(seat);
+        //        seat - tiket
+//        TODO: asegurarse que row 1 es el 0
+        if(ticket.getSeatCategory().compareTo(plane.getRows().get(row-1).getCategory()) > 0 )
+            throw new SeatCategoryIsToHighException();
+
+        ticket.setSeatLocation(new Ticket.SeatLocation(row, column));
 
     }
 
--- server/src/main/java/ar/edu/itba/pod/server/services/AdminServiceImpl.java
+++ server/src/main/java/ar/edu/itba/pod/server/services/AdminServiceImpl.java
@@ -49,7 +49,7 @@ public class AdminServiceImpl implements AdminService {
     }
 
     @Override
-    public void rescheduleTickets() throws RemoteException {
-
+    public ReticketingReport rescheduleTickets() throws RemoteException {
+        return null;
     }
 }
