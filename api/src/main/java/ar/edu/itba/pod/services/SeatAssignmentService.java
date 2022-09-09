8e9788f8810ce7b8ba5b92b7f1d1b1ff7906f276
luciatorrusio <lucia.torrusio@gmail.com>
2022-09-08 23:38:15 -0300

    change ticket implemented

--- api/src/main/java/ar/edu/itba/pod/services/SeatAssignmentService.java
+++ api/src/main/java/ar/edu/itba/pod/services/SeatAssignmentService.java
@@ -10,6 +10,6 @@ public interface SeatAssignmentService {
     void assignSeat(String flightCode, String passenger, int row, char column) throws Exception;
     void changeSeat(String flightCode, String passenger, int row, char column) throws Exception;
     List<Flight> getAlternativeFlights(String flightCode, String passenger);
-    void changeTicket(String passenger, String flightCode, String newFlightCode);
+    void changeTicket(String passenger, String flightCode, String newFlightCode) throws Exception;
 
 }
