f7f5147e383a794b6cef3566d87b856bb2fe42dc
Julian Arce <juarce@itba.edu.ar>
2022-09-10 23:47:07 -0300

    feat: synchronize seat map service implementation

--- server/src/main/java/ar/edu/itba/pod/server/services/SeatMapServiceImpl.java
+++ server/src/main/java/ar/edu/itba/pod/server/services/SeatMapServiceImpl.java
@@ -27,31 +27,27 @@ public class SeatMapServiceImpl implements SeatMapService {
 
     @Override
     public List<SeatRow> getSeatMap(String flightCode, Criteria criteria) throws RemoteException {
-
         //Primero encontrar el vuelo segun el string dado.
         Flight flight = Optional.ofNullable(flights.get(flightCode)).orElseThrow(FlightDoesNotExistException::new);
 
         //Filtrar dentro de los asientos de acuerdo al criterio recibido.
-
         //Pedimos los seats que ya estan asignados
-
-
         final Map<Ticket.SeatLocation, String> assignees = new HashMap<>();
 
-        flight.getTickets().stream()
-                .filter( t -> t.getSeatLocation().isPresent()).
-                forEach( t -> assignees.put(t.getSeatLocation().orElse(null),t.getPassengerName()));
+        synchronized (flight.getTickets()) {
+            flight.getTickets().stream()
+                    .filter(t -> t.getSeatLocation().isPresent()).
+                    forEach(t -> assignees.put(t.getSeatLocation().orElse(null), t.getPassengerName()));
+        }
 
         //Creamos las rows
-
         final List<Plane.RowDescription> rawRows = flight.getPlane().getRows();
 
         //Creamos la coleccion de filas con los seats, chequeando cuando corresponde que esten asignados
-
         List<SeatRow> seatRows = new ArrayList<>();
-        rawRows.forEach(r -> seatRows.add(new SeatRow(r.getRow(), createNewSeats(r.getColumns()), r.getCategory())));
 
-        assignees.forEach( (k,v)-> seatRows.get(k.getRow() - 1).getSeats()[(int) k.getColumn()-'A'].setPassenger(v));
+        rawRows.forEach(r -> seatRows.add(new SeatRow(r.getRow(), createNewSeats(r.getColumns()), r.getCategory())));
+        assignees.forEach((k, v) -> seatRows.get(k.getRow() - 1).getSeats()[(int) k.getColumn() - 'A'].setPassenger(v));
 
         return criteria.filter(seatRows);
     }
